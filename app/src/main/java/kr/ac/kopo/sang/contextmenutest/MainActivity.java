c7998d021389fe6c4f10565d7793de10dbd62e2d
AISW-509-204 <banbansolt@gmail.com>
2026-05-28 14:08:36 +0900

    대화상자

--- app/src/main/java/kr/ac/kopo/sang/contextmenutest/MainActivity.java
+++ app/src/main/java/kr/ac/kopo/sang/contextmenutest/MainActivity.java
@@ -1,5 +1,6 @@
 package kr.ac.kopo.sang.contextmenutest;
 
+import android.content.DialogInterface;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -8,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -25,8 +27,7 @@ public class MainActivity extends AppCompatActivity
     float rotationDegree;   // 수정
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -39,22 +40,30 @@ public class MainActivity extends AppCompatActivity
         btnZoomin = findViewById(R.id.btn_change);
         linear = findViewById(R.id.main);
         Button btnAlert = findViewById(R.id.btn_alert);
-        btnAlert.setOnClickListener(new View.OnClickListener()
-        {
+        btnAlert.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
+                // 대화상자내의 메시지를 "배경색을 파랑색으로 변경할까요?" 로 변경
+                // 대화상자내의 확인 버튼을 클릭: 액티비티의 LinearLayout 배경색 파랑으로 변경
+                // 대화상자내의 취소 버튼을 클릭: 배경색 변화 없음
                 AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
-                dialog.setTitle("대화상자연습");
-                dialog.setMessage("대화상자 내의 내용 부분입니다");
+                dialog.setTitle("배경색 변경");
+                dialog.setMessage("배경색을 파랑색으로 변경할까요?");
                 dialog.setIcon(R.drawable.icon);
+                dialog.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(), "확인 버튼을 클릭했어요.", Toast.LENGTH_SHORT).show();
+                        linear.setBackgroundColor(Color.BLUE);
+                    }
+                });
+                dialog.setNegativeButton("취소", null);
                 dialog.show();
             }
         });
 
         registerForContextMenu(btnRotation);
         registerForContextMenu(btnZoomin);
-
     }
 
     @Override
